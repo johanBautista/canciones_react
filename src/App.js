40532e34663ad9c26455acc26d0e29b5ae9871ac
johan Bautista <jsbparra@gmail.com>
2020-05-18 01:45:39 +0200

    comunicacion api letras

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import Formulario from './components/Formulario';
 
 function App() {
   const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+
+  useEffect(() => {
+    if (Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const resultado = await axios.get(url);
+
+      guardarLetra(resultado.data.lyrics);
+    };
+
+    consultarApiLetra();
+  }, [busquedaletra]);
+
   return (
     <Fragment>
       <div className="section">
