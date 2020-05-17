39e63074a5d3204eca8c2b9481463123ce0e8395
johan Bautista <jsbparra@gmail.com>
2020-05-17 12:25:56 +0200

    comunicacion de form y app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
   return (
     <Fragment>
       <div className="section">
-        <Formulario titulo="Buscador Letra Canciones" />
+        <Formulario
+          titulo="Buscador Letra Canciones"
+          guardarBusquedaLetra={guardarBusquedaLetra}
+        />
       </div>
       <div className="container mt-5">
         <div className="row">
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Error from './Error';
 
-const Formulario = ({ titulo }) => {
+const Formulario = ({ titulo, guardarBusquedaLetra }) => {
   const [busqueda, guardarBusqueda] = useState({
     artista: '',
     cancion: '',
@@ -21,6 +21,7 @@ const Formulario = ({ titulo }) => {
       return;
     }
     guardarError(false);
+    guardarBusquedaLetra(busqueda);
   };
 
   return (
