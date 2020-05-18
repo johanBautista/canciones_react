1e6b25a8162a26411358021a152c541cf81882d1
johan Bautista <jsbparra@gmail.com>
2020-05-18 02:59:25 +0200

    refact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios';
 import Formulario from './components/Formulario';
 import Letra from './components/Letra';
+import axios from 'axios';
 import Informacion from './components/Informacion';
 
 function App() {
   const [busquedaletra, guardarBusquedaLetra] = useState({});
   const [letra, guardarLetra] = useState('');
-  const [informacion, guardarInfo] = useState({});
+  const [info, guardarInfo] = useState({});
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
@@ -17,30 +17,25 @@ function App() {
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
       const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      const [letra, informacion] = await Promise.all([
-        axios.get(url),
-        axios.get(url2),
-      ]);
+      const [letra, informacion] = await Promise.all([axios(url), axios(url2)]);
 
       guardarLetra(letra.data.lyrics);
-      guardarInfo(informacion.data.artist);
+      guardarInfo(informacion.data.artists[0]);
     };
-
     consultarApiLetra();
-  }, [busquedaletra]);
+  }, [busquedaletra, info]);
 
   return (
     <Fragment>
-      <div className="section">
-        <Formulario
-          titulo="Buscador Letra Canciones"
-          guardarBusquedaLetra={guardarBusquedaLetra}
-        />
-      </div>
+      <Formulario
+        titulo="Buscador Letra Canciones"
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
+
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-6">
-            <Informacion informacion={informacion} />
+            <Informacion info={info} />
           </div>
           <div className="col-md-6">
             <Letra letra={letra} />
--- src/components/Informacion.js
+++ src/components/Informacion.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Informacion = ({ informacion }) => {
-  if (Object.keys(informacion).length === 0) return null;
-  const { strArtistThumb, strGenre, strBiographyES } = informacion;
+const Informacion = ({ info }) => {
+  if (Object.keys(info).length === 0) return null;
+
+  const { strArtistThumb, strGenre, strBiographyES } = info;
+
   return (
     <div className="card border-light">
       <div className="card-header bg-primary text-light font-weight-bold">
@@ -15,21 +17,21 @@ const Informacion = ({ informacion }) => {
         <p className="card-text">{strBiographyES}</p>
         <p className="card-text">
           <a
-            href={`https://${informacion.strFacebook}`}
+            href={`https://${info.strFacebook}`}
             target="_blank"
             rel="noopener noreferrer"
           >
             <i className="fab fa-facebook"></i>
           </a>
           <a
-            href={`https://${informacion.strTwitter}`}
+            href={`https://${info.strTwitter}`}
             target="_blank"
             rel="noopener noreferrer"
           >
             <i className="fab fa-twitter"></i>
           </a>
           <a
-            href={`${informacion.strLastFMChart}`}
+            href={`${info.strLastFMChart}`}
             target="_blank"
             rel="noopener noreferrer"
           >
